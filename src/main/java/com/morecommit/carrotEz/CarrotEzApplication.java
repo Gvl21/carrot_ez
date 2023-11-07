2238ecaf366da30f344f15f5023678d276f9342a
Gvl21 <thornyk1@gmail.com>
2023-11-07 12:47:24 +0900

    first commit

---
+++ src/main/java/com/morecommit/carrotEz/CarrotEzApplication.java
@@ -0,0 +1,13 @@
+package com.morecommit.carrotEz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotEzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotEzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morecommit/carrotEz/CarrotEzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morecommit.carrotEz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotEzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
