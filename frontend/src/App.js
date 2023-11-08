a83219926eba51d46f97d5c86f837f03621f1d82
Gvl21 <thornyk1@gmail.com>
2023-11-08 10:29:25 +0900

    add 테스트용 리액트 빌드파일 spring resource/static에 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, React } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [msg, setMsg] = useState(' ');
+
+    useEffect(() => {
+        fetch('/hello')
+            .then((res) => res.text())
+            .then((txt) => setMsg(txt));
+    });
+
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <p>{msg}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
