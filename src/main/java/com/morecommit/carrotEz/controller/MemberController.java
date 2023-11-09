b96e29772687d44da565d2c9977f47f583b554b5
dlrkdmsdlrkdmsdlrkdms <gaeun0006@gmail.com>
2023-11-09 12:32:46 +0900

    MemberController 에 saveMember 추가 및

--- src/main/java/com/morecommit/carrotEz/controller/MemberController.java
+++ src/main/java/com/morecommit/carrotEz/controller/MemberController.java
@@ -1,10 +1,11 @@
 package com.morecommit.carrotEz.controller;
 
 import com.morecommit.carrotEz.dto.MemberDto;
-import com.morecommit.carrotEz.repository.MemberRepository;
+import com.morecommit.carrotEz.entity.Member;
 import com.morecommit.carrotEz.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+
 public class MemberController {
     private final MemberService memberService;
-
-    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/members/new")
     public String memberForm(Model model){
@@ -29,8 +30,28 @@ public class MemberController {
         if (bindingResult.hasErrors()) {
             return "member/memberForm";
         }
-        return null;
+        try {
+            // 에러 있으면 다시 회원가입 페이지로 돌아감
+            Member member = Member.createMember(memberDto, passwordEncoder);
+            // 엔티티에서 db에 저장
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+        return "redirect:/";
     }
 
+    // 로그인
+    @GetMapping("/members/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
 
+    @GetMapping("/members/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg",
+                "아이디와 비밀번호를 확인 해주세요");
+        return "/member/memberLoginForm";
+    }
 }
--- src/main/java/com/morecommit/carrotEz/service/MemberService.java
+++ src/main/java/com/morecommit/carrotEz/service/MemberService.java
@@ -1,6 +1,11 @@
 package com.morecommit.carrotEz.service;
 
+import com.morecommit.carrotEz.entity.Member;
+
 public class MemberService {
+    public void saveMember(Member member) {
+
+    }
 
     // 회원가입
 
