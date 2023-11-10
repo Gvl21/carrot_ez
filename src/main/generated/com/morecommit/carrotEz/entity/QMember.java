ed4bfb51083ba00ddf660fa48d869d808f9077fa
Gvl21 <thornyk1@gmail.com>
2023-11-10 10:28:20 +0900

    추가) 깃 프론트 리포지토리 서브모듈 추가

---
+++ src/main/generated/com/morecommit/carrotEz/entity/QMember.java
@@ -0,0 +1,47 @@
+package com.morecommit.carrotEz.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = 959501188L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final EnumPath<com.morecommit.carrotEz.constant.Address> address = createEnum("address", com.morecommit.carrotEz.constant.Address.class);
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath password = createString("password");
+
+    public final EnumPath<com.morecommit.carrotEz.constant.Role> role = createEnum("role", com.morecommit.carrotEz.constant.Role.class);
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
---
+++ src/main/java/com/morecommit/carrotEz/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.morecommit.carrotEz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfig(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")  // 모든 경로
+                        .allowedMethods("*")            // 모든 메서드
+                        .allowedOrigins("http://localhost:3030");       // 접근 가능 출처(리액트)
+            }
+        };
+    }
+}
