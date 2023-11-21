33d058442cd3d361aca267cd8f298501e6da344e
dlrkdmsdlrkdmsdlrkdms <gaeun0006@gmail.com>
2023-11-21 12:20:05 +0900

    FileController 오류 해결

--- src/main/java/com/morecommit/carrotEz/controller/FileController.java
+++ src/main/java/com/morecommit/carrotEz/controller/FileController.java
@@ -1,4 +1,29 @@
 package com.morecommit.carrotEz.controller;
 
+import com.morecommit.carrotEz.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/file")
+@RequiredArgsConstructor
 public class FileController {
+
+    private final FileService fileService;
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        String url = fileService.upload(file);
+        return url;
+    }
+
+    @GetMapping(value = "{fileName}", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_GIF_VALUE})
+    public Resource getImage(
+            @PathVariable("fileName") String fileName) {
+        Resource resource = fileService.getImage(fileName);
+        return resource;
+    }
 }
