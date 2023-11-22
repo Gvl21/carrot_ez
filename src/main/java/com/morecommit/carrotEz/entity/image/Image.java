89b220ecc1267258bdaec93fc29d2cb8b6354b99
dlrkdmsdlrkdmsdlrkdms <gaeun0006@gmail.com>
2023-11-22 15:52:05 +0900

    imqge 엔티티 파일 생성 및 작성 완료

---
+++ src/main/java/com/morecommit/carrotEz/entity/image/Image.java
@@ -0,0 +1,21 @@
+package com.morecommit.carrotEz.entity.image;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity(name = "Image")
+@Table(name = "Image")
+
+public class Image {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int sequence;
+    private int boardNumber;
+    private String image;
+
+}
